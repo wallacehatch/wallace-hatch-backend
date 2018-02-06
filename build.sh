a91178843a000b8d4333456f39072a3a9806b069
gregm711 <gregm@wearevest.com>
2018-02-06 14:59:53 -0500

    coming along

--- build.sh
+++ build.sh
@@ -8,3 +8,15 @@ cp -a email-templates/ docker/email-templates/
 
 # docker stop $(docker ps -a -q)
 # docker run -p 80:8090 -d --restart always demo-api-updated
+
+
+export KUBE_CA_CERT=$(kubectl config view --flatten --output=json \
+       | jq --raw-output '.clusters[2] .cluster ["certificate-authority-data"]')
+export KUBE_ENDPOINT=$(kubectl config view --flatten --output=json \
+       | jq --raw-output '.clusters[2] .cluster ["server"]')
+export KUBE_ADMIN_CERT=$(kubectl config view --flatten --output=json \
+       | jq --raw-output '.users[2] .user ["client-certificate-data"]')
+export KUBE_ADMIN_KEY=$(kubectl config view --flatten --output=json \
+       | jq --raw-output '.users[2] .user ["client-key-data"]')
+export KUBE_USERNAME=$(kubectl config view --flatten --output=json \
+       | jq --raw-output '.users[2] .user ["username"]')
